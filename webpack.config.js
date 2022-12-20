3c83a3ffba65403a748a2dcf95385e81960147c3
Geojorge <jorgezevallosr@gmail.com>
2022-12-20 18:15:32 -0500

    Inicio webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
