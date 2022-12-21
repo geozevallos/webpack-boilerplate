7193f1b1750460a6b15f0ec76df6495d68368a8a
Geojorge <jorgezevallosr@gmail.com>
2022-12-20 19:21:36 -0500

    estilos de manera global

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import {miFunction} from './js/components'
-
+import './styles.css'
 
 miFunction();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
+    // mode: 'development',
+    optimization:{
+        minimizer: [new CssMinimizerPlugin(),]
+    },
     module: {
         rules: [
+            // css dinámico
             {
                 test: /\.css$/i,
+                exclude: /styles\.css$/,
                 use: ["style-loader", "css-loader"],
+            },
+            // css especifico
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
             },
             {
                 test: /\.html$/,
@@ -18,6 +34,10 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: './src/index.html',
             filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
         })
     ]
 }
